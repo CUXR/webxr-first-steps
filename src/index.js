96bcc1b8e98794c3b183f8c94a148e1d2c6eadb2
Temi <ta375@cornell.edu>
2024-10-22 17:41:18 -0400

    Created Basic 3D Objects

--- src/index.js
+++ src/index.js
@@ -40,50 +40,35 @@ function updateScoreDisplay() {
 }
 
 function setupScene({ scene, camera, renderer, player, controllers }) {
-	const gltfLoader = new GLTFLoader();
+	// floor
+	const floorGeometry = new THREE.PlaneGeometry(6, 6);
+	const floorMaterial = new THREE.MeshStandardMaterial({color: 'white'});
+	const floor = new THREE.Mesh(floorGeometry, floorMaterial);	
+	floor.rotateX(-Math.PI / 2);
+	scene.add(floor);
 
-	gltfLoader.load('assets/spacestation.glb', (gltf) => {
-		scene.add(gltf.scene);
-	});
+	// cone
+	const coneGeometry = new THREE.ConeGeometry(0.6, 1.5);
+	const coneMaterial = new THREE.MeshStandardMaterial({ color: 'purple' });
+	const cone = new THREE.Mesh(coneGeometry, coneMaterial);
+	scene.add(cone);
+	cone.position.set(0.4, 0.75, -1.5);
 
-	gltfLoader.load('assets/blaster.glb', (gltf) => {
-		blasterGroup.add(gltf.scene);
-	});
+	// cube
+	const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+	const cubeMaterial = new THREE.MeshStandardMaterial({ color: 'orange' });
+	const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+	scene.add(cube);
+	cube.position.set(-0.8, 0.5, -1.5);
+	cube.rotateY(Math.PI / 4);
 
-	gltfLoader.load('assets/target.glb', (gltf) => {
-		for (let i = 0; i < 3; i++) {
-			const target = gltf.scene.clone();
-			target.position.set(
-				Math.random() * 10 - 5,
-				i * 2 + 1,
-				-Math.random() * 5 - 5,
-			);
-			scene.add(target);
-			targets.push(target);
-		}
-	});
-
-	scene.add(scoreText);
-	scoreText.position.set(0, 0.67, -1.44);
-	scoreText.rotateX(-Math.PI / 3.3);
-	updateScoreDisplay();
-
-	// Load and set up positional audio
-	const listener = new THREE.AudioListener();
-	camera.add(listener);
-
-	const audioLoader = new THREE.AudioLoader();
-	laserSound = new THREE.PositionalAudio(listener);
-	audioLoader.load('assets/laser.ogg', (buffer) => {
-		laserSound.setBuffer(buffer);
-		blasterGroup.add(laserSound);
-	});
-
-	scoreSound = new THREE.PositionalAudio(listener);
-	audioLoader.load('assets/score.ogg', (buffer) => {
-		scoreSound.setBuffer(buffer);
-		scoreText.add(scoreSound);
-	});
+	// sphere
+	const sphereGeometry = new THREE.SphereGeometry(0.4);
+	const sphereMaterial = new THREE.MeshStandardMaterial({ color: 'red' });
+	const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	scene.add(sphere);
+	sphere.position.set(0.6, 0.4, -0.5);
+	sphere.scale.set(1.2, 1.2, 1.2);
 }
 
 function onFrame(
